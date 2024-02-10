59c1e1f978b6341ffa73cf2f0372f2ef9c6f1d96
Grzegorz <kpv135@gmail.com>
2024-02-10 21:21:51 +0100

    reformat MenuScreen and update requirements.txt

--- Screens/MenuScreen.py
+++ Screens/MenuScreen.py
@@ -9,6 +9,7 @@ class MenuScreen:
     __LANE_HEIGHT = 10
     __menu_list = ['STATS', 'CLOCK', 'SETTINGS', 'EXIT']
     __first_printed_element = 0
+    __current_index = 0
 
     def __init__(self, disp: SSD1306_I2C, input_event: KeyEvent):
         self.__font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 10)
@@ -17,33 +18,31 @@ class MenuScreen:
 
     def run(self):
         button = None
-        current_index = 0
         while not (button is not None and (button is Button.RETURN)):
-            current_index = self.__scroll_cursor(current_index, button)
+            self.__scroll_cursor(button)
 
             image = Image.new("1", (self.__disp.width, self.__disp.height))
             draw = ImageDraw.Draw(image)
-            self.__prepare_list(draw, current_index)
+            self.__prepare_list(draw)
             self.__disp.image(image)
             self.__disp.show()
             button = self.__inputEvent.wait()
 
         print("end of clock screen")
 
-    def __scroll_cursor(self, current_index, button):
+    def __scroll_cursor(self, button):
         if button is Button.UP:
-            current_index -= 1
-            if current_index < 0:
-                current_index = len(self.__menu_list) - 1
+            self.__current_index -= 1
+            if self.__current_index < 0:
+                self.__current_index = len(self.__menu_list) - 1
         elif button is Button.DOWN:
-            current_index += 1
-            if current_index > len(self.__menu_list) - 1:
-                current_index = 0
-        return current_index
+            self.__current_index += 1
+            if self.__current_index > len(self.__menu_list) - 1:
+                self.__current_index = 0
 
-    def __prepare_list(self, draw, current_index):
+    def __prepare_list(self, draw):
         for i, element in enumerate(self.__menu_list):
-            if i == current_index:
+            if i == self.__current_index:
                 label = '->' + element
             else:
                 label = '  ' + element
