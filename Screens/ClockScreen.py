a9ec488e72156638108d3beda0392c43e9b1f572
Grzegorz <kpv135@gmail.com>
2024-02-11 15:58:06 +0100

    update ClockScreen add DisableScreen reformat MenuScreen and main class

--- Screens/ClockScreen.py
+++ Screens/ClockScreen.py
@@ -10,9 +10,10 @@ from Keyboard.KeyEvent import KeyEvent
 class ClockScreen:
 
     def __init__(self, disp: SSD1306_I2C, input_event: KeyEvent):
-        self.__font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 12)
+        self.__font = ImageFont.truetype('/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf', 26)
         self.__disp = disp
         self.__inputEvent = input_event
+        self.__SLEEP_TIME = 0.2
 
     def run(self):
         button = None
@@ -22,6 +23,7 @@ class ClockScreen:
             draw.text((0, 1 + 0), time.strftime("%H:%M:%S", time.gmtime()), font=self.__font, fill=255)
             self.__disp.image(image)
             self.__disp.show()
-            button = self.__inputEvent.wait(0.25)
+
+            button = self.__inputEvent.wait(self.__SLEEP_TIME)
 
         print("end of clock screen")
